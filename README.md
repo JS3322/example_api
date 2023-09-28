c79f6fb58efe04292e86cc596ea1b9e4933e39bc
js3322 <js@cleancode.kr>
2023-09-29 01:24:39 +0900

    FEAT #0 Init

---
+++ README.md
@@ -0,0 +1,10 @@
+# monitoring_in_action
+- [outline](#outline)
+
+---
+# outline
+api 구축
+
+### example_api : work
+- swagger+ openAPI3.0 사용 설정
+- api example
---
+++ src/test/java/com/cleancode/example_api/ExampleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleancode.example_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
